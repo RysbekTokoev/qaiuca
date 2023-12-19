4514e7f3f36c49126655a488ffd0ce352536fa64
rtokoev <rtokoev@nurtelecom.kf>
2023-12-19 11:59:52 +0600

    configs updated + gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require('cypress')
 
-
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'http://167.114.201.175:5000/',
+        setupNodeEvents(on, config) {
+
+        },
+        viewportHeight: 1080,
+        viewportWidth: 1920,
         chromeWebSecurity: false,
-        supportFile: false,
       },
+
     env: {
         login: "default",
         password: "default",
